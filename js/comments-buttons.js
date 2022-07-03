e558704bb01456237a2900c25b72e9485e119af6
caoyusong <caoyusong9527@163.com>
2022-07-04 07:29:30 +0800

    Site updated: 2022-07-04 07:29:29

--- js/comments-buttons.js
+++ js/comments-buttons.js
@@ -1,23 +1,34 @@
 /* global CONFIG */
 
-(function() {
-  const commentButton = document.querySelectorAll('.comment-button');
-  commentButton.forEach(element => {
+(function () {
+  const commentButton = document.querySelectorAll(".comment-button");
+  commentButton.forEach((element) => {
     const commentClass = element.classList[2];
-    element.addEventListener('click', () => {
-      commentButton.forEach(active => active.classList.toggle('active', active === element));
-      document.querySelectorAll('.comment-position').forEach(active => active.classList.toggle('active', active.classList.contains(commentClass)));
+    element.addEventListener("click", () => {
+      commentButton.forEach((active) =>
+        active.classList.toggle("active", active === element)
+      );
+      document
+        .querySelectorAll(".comment-position")
+        .forEach((active) =>
+          active.classList.toggle(
+            "active",
+            active.classList.contains(commentClass)
+          )
+        );
       if (CONFIG.comments.storage) {
-        localStorage.setItem('comments_active', commentClass);
+        localStorage.setItem("comments_active", commentClass);
       }
     });
   });
   let { activeClass } = CONFIG.comments;
   if (CONFIG.comments.storage) {
-    activeClass = localStorage.getItem('comments_active') || activeClass;
+    activeClass = localStorage.getItem("comments_active") || activeClass;
   }
   if (activeClass) {
-    const activeButton = document.querySelector(`.comment-button.${activeClass}`);
+    const activeButton = document.querySelector(
+      `.comment-button.${activeClass}`
+    );
     if (activeButton) {
       activeButton.click();
     }
